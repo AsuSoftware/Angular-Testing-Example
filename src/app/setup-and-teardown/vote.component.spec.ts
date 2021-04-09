8a8f7af134adf04b26fa889a93860a8e300a5065
Antonio Popa <web.antonio.popa@gmail.com>
2021-04-09 13:08:06 +0200

    Initial commit

---
+++ src/app/setup-and-teardown/vote.component.spec.ts
@@ -0,0 +1,21 @@
+import { VoteComponent } from './vote.component';
+
+describe('VoteComponent', () => {
+
+  let component: VoteComponent;
+
+  // Usiamo questo metodo perchè ogni test(it()) dovrebbe essere isolato, cioè l'impatto di un it su quel componente non dovrebbe riflettere sugli altri
+  beforeEach(() => { // Questo metodo viene eseguito prima di ogni test
+    component = new VoteComponent(); // Arrange
+  });
+
+  it('should increment totalVotes when upvoted', () => {
+    component.upVote(); // Act
+    expect(component.totalVotes).toBe(1); // Assert
+  });
+
+  it('should decrement totalVotes when downvotes', () => {
+    component.downVote(); // Act
+    expect(component.totalVotes).toBe(1); // Assert
+  });
+});
